c8a512f98b52a0db0fbc12e6219e963809af0ff3
Daniel Marhuenda <alu0101487731@ull.edu.es>
2025-03-10 10:40:51 +0000

    Hecho gasta map

---
+++ src/modificacion/generica.ts
@@ -0,0 +1,110 @@
+
+interface Listable<T>{
+    append(lista2:Lista<T>):void;
+    concatenate(listas:Lista<T>[]):void;
+    filter(predicado:Function):Lista<T>;
+    lenght():number;
+    map(funcion:Function):Lista<T>;
+    //reduce(lista:Lista<T>, funcion:Function, acumulador:number):void;
+    //reverse(lista:Lista<T>):Lista<T>;
+    //forEach(lista:Lista<T>, funcion:Function):No se
+}
+
+/**
+ * Clase generica para representar listas
+ * @template T Cualquier tipo de variable
+ * @class Es una clase
+ */
+export class Lista<T> implements Listable<T>{
+
+    /**
+     * Constructor de la clase genérica Lista
+     * @param items Valores iniciales de la lista
+     */
+    constructor(private items: T[]){
+
+    }
+
+    /**
+     * Getter de los items para los tests
+     * @returns Array internos de los items
+     */
+    getitems():T[]{
+        return this.items;
+    }
+
+    /**
+     * Funcion que dadas dos listas devuelve una lista con los valores de la segunda lista
+     * puestos después de los valores de la primera lista
+     * @param lista2 Segunda lista
+     */
+    append(lista2: Lista<T>): void{
+        let iterador_pos_segundo:number = 0;
+        while(lista2.items[iterador_pos_segundo] !== undefined){
+            this.items.push(lista2.items[iterador_pos_segundo]);
+            iterador_pos_segundo++;
+        }
+
+    }
+
+    /**
+     * Funcion que dadas varias listas junto todos sus valores
+     * @param listas Listas a unir
+     */
+    concatenate(listas: Lista<T>[]): void{
+        let iterador_listas = 0;
+
+        while(listas[iterador_listas] !== undefined){
+            this.append(listas[iterador_listas]);
+            iterador_listas++;
+        }
+    }
+
+    /**
+     * Funcion que filtra la lista según una funcion booleana dada
+     * @param funcion Funcion filtradora
+     * @returns Lista ya filtrada
+     */
+    filter(funcion: (a: T) => boolean):Lista<T>{
+        let resultado:Lista<T> = new Lista<T>([]);
+        let iterador_pos = 0;
+
+        while(this.items[iterador_pos] !== undefined){
+            if(funcion(this.items[iterador_pos])) resultado.items.push(this.items[iterador_pos]);
+
+            iterador_pos++;
+        }
+
+        return resultado;
+    }
+
+
+    /**
+     * Función que devuelve la cantidad de valores dentro de la lista
+     * @returns Longitud de la lista
+     */
+    lenght(): number {
+        let iterador_pos = 0;
+        while(this.items[iterador_pos] !== undefined){
+            iterador_pos++;
+        }
+        return iterador_pos;
+    }
+
+    /**
+     * Funcion que aplica a todos los valores de la lista una funcion
+     * @param funcion Funcion a aplicar
+     * @returns Lista con los valores ya aplicados
+     */
+    map(funcion: Function): Lista<T> {
+        let resultado:Lista<T> = new Lista<T>([]);
+        let iterador_pos = 0;
+
+        while(this.items[iterador_pos] !== undefined){
+            resultado.items.push(funcion(this.items[iterador_pos]));
+            iterador_pos++;
+        }
+
+        return resultado;
+    }
+}
---
+++ tests/modificacion/generica.spec.ts
@@ -0,0 +1,70 @@
+import {describe, expect, test} from "vitest";
+import {Lista} from "../../src/modificacion/generica"
+
+describe("Tests de la modificacon", () => {
+    let listanumber1:Lista<number> = new Lista<number>([1,2,3]);
+    let listanumber2:Lista<number> = new Lista<number>([3,4,5]);
+    let listanumber3:Lista<number> = new Lista<number>([5,6,7]);
+
+    test("Append number", () => {
+        let listanumbertest:Lista<number> = new Lista<number>([1,2,3]);
+        listanumbertest.append(listanumber2);
+        expect(listanumbertest.getitems()).toStrictEqual([1,2,3,3,4,5]);
+    });
+
+    test("Concatenate number", () => {
+        let listanumbertest:Lista<number> = new Lista<number>([1,2,3]);
+        listanumbertest.concatenate([listanumber2,listanumber3])
+        expect(listanumbertest.getitems()).toStrictEqual([1,2,3,3,4,5,5,6,7]);
+    });
+
+    test("filter number", () => {
+        expect(listanumber1.filter((item) => item>2).getitems()).toStrictEqual([3]);
+        expect(listanumber1.filter((item) => item<3).getitems()).toStrictEqual([1,2]);
+    });
+
+    test("lenght number", () => {
+        expect(listanumber1.lenght()).toBe(3);
+    });
+
+    test("map number", () => {
+        let listanumbertest:Lista<number> = new Lista<number>([1,2,3]);
+        expect(listanumbertest.map((item:number) => item*2).getitems()).toStrictEqual([2,4,6]);
+    });
+
+
+    let listastring1:Lista<string> = new Lista<string>(["a","b","c"]);
+    let listastring2:Lista<string> = new Lista<string>(["c","d","e"]);
+    let listastring3:Lista<string> = new Lista<string>(["e","f","g"]);
+
+    test("Append string", () => {
+        let listastringtest:Lista<string> = new Lista<string>(["a","b","c"]);
+        listastringtest.append(listastring2);
+        expect(listastringtest.getitems()).toStrictEqual(["a","b","c","c","d","e"]);
+    });
+
+    test("Concatenate string", () => {
+        let listastringtest:Lista<string> = new Lista<string>(["a","b","c"]);
+        listastringtest.concatenate([listastring2,listastring3])
+        expect(listastringtest.getitems()).toStrictEqual(["a","b","c","c","d","e", "e","f","g"]);
+    });
+
+    test("filter string", () => {
+        expect(listastring1.filter((item) => item === "b").getitems()).toStrictEqual(["b"]);
+        expect(listastring1.filter((item) => item !== "b").getitems()).toStrictEqual(["a", "c"]);
+    });
+
+    test("lenght string", () => {
+        expect(listastring1.lenght()).toBe(3);
+    });
+
+    test("map string", () => {
+        let listastringtest:Lista<string> = new Lista<string>(["a","b","c"]);
+        expect(listastringtest.map((item:string) => item+"a").getitems()).toStrictEqual(["aa","ba","ca"]);
+    });
+
+
+    
+
+
+});
